c6331bc1bdfc08bc9adea32663b7e2869229ce53
xiyao <purewheat@gmail.com>
2020-05-13 03:31:47 +0800

    vuejs 通过 axios 访问api,使用element ui

---
+++ front_end/src/components/Ping.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Ping',
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+      getMessage(){
+          const path = 'http://localhost:5000/api/ping'
+          axios.get(path)
+            .then((res)=>{
+                this.msg = res.data;
+            })
+            .catch((error)=>{
+                console.error(error);
+            })
+      }
+  },
+  created(){
+      this.getMessage()
+  }
+}
+</script>
